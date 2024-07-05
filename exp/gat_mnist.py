fix batching and masking, add coordinate slic transformer

exp/gat_mnist.py
@@ -1,5 +1,3 @@
-import os
-
 from torch.utils.data import random_split, DataLoader
 from torch.optim import Adam
 from torch.nn import CrossEntropyLoss
@@ -10,9 +8,9 @@ from torchvision.transforms import Compose, Resize, ToTensor, Normalize
 
 from src.utils.training.common import device
 from src.utils.training.image_classification import train_test_loop
-from src.models.vit import ViT
+from src.models.slic_gat import SLICGAT
 
-save_path = "./data/models/vit_mnist/"
+save_path = "./data/models/gat_mnist/"
         
 transform = Compose([
     Resize((28, 28)),
@@ -33,7 +31,7 @@ classes = 10
 num_layers = 3
 hidden_size = 256
 dropout = 0.2
-model = ViT(image_size, channel_size, patch_size, embed_size, num_heads, classes, num_layers, hidden_size, dropout=dropout, learnable_pe=False).to(device)
+model = ViT(image_size, channel_size, patch_size, embed_size, num_heads, classes, num_layers, hidden_size, dropout=dropout).to(device)
 
 optimizer = Adam(model.parameters(), lr=5e-5)
 criterion = CrossEntropyLoss()

src/models/slic_transformer.py
@@ -5,6 +5,7 @@ import scipy.io as sio
 import torch
 from PIL import Image
 from torch.utils import data
+from torchvision.transforms import Resize
 
 import random
 import scipy
@@ -22,23 +23,8 @@ import matplotlib.pyplot as plt
 from torch import nn
 import torch.nn.functional as F
 
-from .vit import VisionEncoder
-
-
-def segment_image_slic_avg_pool(image, n_segments=14*14, compactness=0.5):
-    assert image.shape[0] == 1
-    
-    image = np.array(image.squeeze(0))
-    
-    seg = slic(image, n_segments=n_segments, compactness=compactness, channel_axis=None)
-    reg = regionprops(seg, image)
-    
-    intensities = torch.tensor(list(map(lambda r: r.mean_intensity, reg))).unsqueeze(-1)
-    positions = torch.tensor(list(map(lambda r: r.centroid, reg)))
-
-    tokens = torch.cat((intensities, positions), dim=-1)
-    
-    return tokens.float()
+from .vit import VisionEncoder, PositionalEncoding2D
+from ..utils.segmentation import segment_image_slic_avg_pool
 
 
 class SLICTransformer(nn.Module):
@@ -57,7 +43,6 @@ class SLICTransformer(nn.Module):
         self.dropout_layer = nn.Dropout(dropout)
         self.embeddings = nn.Linear(in_size, embed_size)
         self.class_token = nn.Parameter(torch.randn(1, 1, embed_size))
-        self.positional_encoding = nn.Parameter(torch.randn(1, num_regions+1, embed_size))
         
         self.encoders = nn.ModuleList([
             VisionEncoder(embed_size, num_heads, hidden_size, dropout) for _ in range(num_layers)
@@ -84,6 +69,86 @@ class SLICTransformer(nn.Module):
         
         return x
     
+
+class CoordViT(nn.Module):
+    r"""Vision Transformer Model
+
+        A transformer model to solve vision tasks by treating images as sequences of tokens.
+
+        Args:
+            image_size      (int): Size of input image
+            channel_size    (int): Size of the channel
+            patch_size      (int): Max patch size, determines number of split images/patches and token size
+            embed_size      (int): Embedding size of input
+            num_heads       (int): Number of heads in Multi-Headed Attention
+            classes         (int): Number of classes for classification of data
+            hidden_size     (int): Number of hidden layers
+            dropout         (float, optional): A probability from 0 to 1 which determines the dropout rate
+
+    """
+
+    def __init__(self, image_size: int, channel_size: int, patch_size: int, embed_size: int, num_heads: int,
+                 classes: int, num_layers: int, hidden_size: int, dropout: float = 0.1):
+        super(CoordViT, self).__init__()
+
+        self.p = patch_size
+        self.image_size = image_size
+        self.embed_size = embed_size
+        self.num_patches = (image_size // patch_size) ** 2
+        self.patch_size = channel_size * (patch_size ** 2)
+        self.num_heads = num_heads
+        self.classes = classes
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.dropout = dropout
+        self.dropout_layer = nn.Dropout(dropout)
+
+        self.embeddings = nn.Linear(self.patch_size, self.embed_size)
+        self.class_token = nn.Parameter(torch.randn(1, 1, self.embed_size))
+        self.positional_encoding = PositionalEncoding2D(self.embed_size, self.image_size)
+
+        self.encoders = nn.ModuleList([])
+        for layer in range(self.num_layers):
+            self.encoders.append(VisionEncoder(self.embed_size, self.num_heads, self.hidden_size, self.dropout))
+
+        self.norm = nn.LayerNorm(self.embed_size)
+
+        self.classifier = nn.Sequential(
+            nn.Linear(self.embed_size, self.classes)
+        )
+
+    def forward(self, x, coords, mask):
+        b, n, c, h, w = x.size()
+
+        x = x.reshape(b, n, h*w)
+        x = self.embeddings(x)
+
+        b, n, e = x.size()
+
+        pe = self.positional_encoding(coords.int()).to(x.device) # TODO int
+        x = x + pe
+        
+        class_token = self.class_token.expand(b, 1, e)
+        x = torch.cat((x, class_token), dim=1)
+        mask = torch.cat((mask, torch.tensor([False] * mask.shape[0], device=mask.device).unsqueeze(1)), dim=1)
+        
+        x = self.dropout_layer(x)
+        
+        for encoder in self.encoders:
+            x = encoder(x, mask)
+
+        x = x[:, -1, :]
+
+        x = F.log_softmax(self.classifier(self.norm(x)), dim=-1)
+
+        return x
+    
+    def to(self, device):
+        super(CoordViT, self).to(device)
+        self.positional_encoding = self.positional_encoding.to(device)
+        return self
+
+
 
 if __name__ == "__main__":
     image = pickle.load(open("./data/image/digit_example_tensor.pkl", "rb"))

src/models/vit.py
@@ -1,7 +1,3 @@
-"""
-Source: https://github.com/UdbhavPrasad072300/Transformer-Implementations
-"""
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -13,104 +9,30 @@ import torchvision.models as models
 import math
 
 
-class CoordinatePE(nn.Module):
-    def __init__(self, dim):
-        super(CoordinatePE, self).__init__()
-        
+class PositionalEncoding2D(nn.Module):
+    def __init__(self, dim, max_len=1000):
+        super(PositionalEncoding2D, self).__init__()
         self.dim = dim
+        
+        self.pe = torch.zeros(max_len, dim)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, dim, 2).float() * (-math.log(10000.0) / dim))
+        self.pe[:, 0::2] = torch.sin(position * div_term)
+        self.pe[:, 1::2] = torch.cos(position * div_term)
     
-    def forward(self, x, coordinates):
-        b, l, _ = x.shape
+        # self.register_buffer("pe_x", pe.clone()) # TODO do we need this?
+        # self.register_buffer("pe_y", pe.clone())
+    
+    def forward(self, coords): # TODO forces ints
+        pe_x = self.pe[coords[..., 0]]
+        pe_y = self.pe[coords[..., 1]]
         
-        pe = torch.zeros(l, self.dim)
-        div = torch.exp()
-        
-
-
-class MultiHeadAttention(nn.Module):
-    r"""Multi-headed Attention for input Query, Key, Value
-
-    Multi-headed Attention is a module for attention mechanisms which runs through attention in several times in
-    parallel, then the multiple outputs are concatenated and linearly transformed
-
-    Args:
-        embed_size  (int): Max embedding size
-        num_heads   (int): Number of heads in multi-headed attention; Number of splits in the embedding size
-        dropout     (float, optional): Percentage of Dropout to be applied in range 0 <= dropout <=1
-        batch_dim   (int, optional): The dimension in which batch dimensions is
-
-    """
-
-    def __init__(self, embed_size: int, num_heads: int, dropout: float = 0.2, batch_dim: int = 0):
-        super(MultiHeadAttention, self).__init__()
-
-        self.embed_size = embed_size
-        self.num_heads = num_heads
-        self.dropout = dropout
-        self.batch_dim = batch_dim
-
-        self.dropout_layer = nn.Dropout(dropout)
-
-        self.head_size = self.embed_size // self.num_heads
-
-        assert self.head_size * self.num_heads == self.embed_size, "Heads cannot split Embedding size equally"
-
-        self.Q = nn.Linear(self.embed_size, self.embed_size)
-        self.K = nn.Linear(self.embed_size, self.embed_size)
-        self.V = nn.Linear(self.embed_size, self.embed_size)
-
-        self.linear = nn.Linear(self.embed_size, self.embed_size)
-
-    def forward(self, q, k, v, mask=None):
-        if self.batch_dim == 0:
-            out = self.batch_0(q, k, v, mask)
-        elif self.batch_dim == 1:
-            out = self.batch_1(q, k, v, mask)
-
-        return out, None
-
-    def batch_0(self, q, k, v, mask=None):
-        q_batch_size, q_seq_len, q_embed_size = q.size()
-        k_batch_size, k_seq_len, k_embed_size = k.size()
-        v_batch_size, v_seq_len, v_embed_size = v.size()
-
-        q = self.Q(q).reshape(q_batch_size, q_seq_len, self.num_heads, self.head_size)
-        k = self.K(k).reshape(k_batch_size, k_seq_len, self.num_heads, self.head_size)
-        v = self.V(v).reshape(v_batch_size, v_seq_len, self.num_heads, self.head_size)
-
-        attention = self.attention(q, k, v, mask=mask)
-        concatenated = attention.reshape(v_batch_size, -1, self.embed_size)
-        out = self.linear(concatenated)
-
-        return out
-
-    def batch_1(self, q, k, v, mask=None):
-        q_seq_len, q_batch_size, q_embed_size = q.size()
-        k_seq_len, k_batch_size, k_embed_size = k.size()
-        v_seq_len, v_batch_size, v_embed_size = v.size()
-
-        q = self.Q(q).reshape(q_seq_len, q_batch_size, self.num_heads, self.head_size).transpose(0, 1)
-        k = self.K(k).reshape(k_seq_len, k_batch_size, self.num_heads, self.head_size).transpose(0, 1)
-        v = self.V(v).reshape(v_seq_len, v_batch_size, self.num_heads, self.head_size).transpose(0, 1)
-
-        attention = self.attention(q, k, v, mask=mask)
-        concatenated = attention.reshape(-1, v_batch_size, self.embed_size)
-
-        out = self.linear(concatenated)
-
-        return out
-
-    def attention(self, q, k, v, mask=None):
-        scores = torch.einsum("bqhe,bkhe->bhqk", [q, k])
-
-        if mask is not None:
-            scores = scores.masked_fill(mask == 0, -1e9)
-
-        scores /= math.sqrt(self.embed_size)
-        scores = F.softmax(scores, dim=-1)
-        scores = self.dropout_layer(scores)
-        attention = torch.einsum("bhql,blhd->bqhd", [scores, v])
-        return attention
+        return pe_x + pe_y
+    
+    def to(self, device):
+        super(PositionalEncoding2D, self).to(device)
+        self.pe = self.pe.to(device)
+        return self
 
 
 class VisionEncoder(nn.Module):
@@ -137,8 +59,7 @@ class VisionEncoder(nn.Module):
         self.norm1 = nn.LayerNorm(self.embed_size)
         self.norm2 = nn.LayerNorm(self.embed_size)
         
-        # self.attention = nn.MultiheadAttention(self.embed_size, self.num_heads, dropout=dropout) # TODO this is freaky
-        self.attention = MultiHeadAttention(self.embed_size, self.num_heads, dropout=dropout)
+        self.attention = nn.MultiheadAttention(self.embed_size, self.num_heads, dropout=dropout)
 
         self.mlp = nn.Sequential(
             nn.Linear(self.embed_size, 4 * self.embed_size),
@@ -148,11 +69,16 @@ class VisionEncoder(nn.Module):
             nn.Dropout(self.dropout)
         )
 
-    def forward(self, x):
+    def forward(self, x, mask=None):
         x = self.norm1(x)
-        attn, _ = self.attention(x, x, x)
+        
+        x = x.transpose(0, 1)
+        attn, _ = self.attention(x, x, x, key_padding_mask=mask)
         x = x + attn
+        x = x.transpose(0, 1)
+        
         x = x + self.mlp(self.norm2(x))
+        
         return x
 
 
@@ -174,7 +100,7 @@ class ViT(nn.Module):
     """
 
     def __init__(self, image_size: int, channel_size: int, patch_size: int, embed_size: int, num_heads: int,
-                 classes: int, num_layers: int, hidden_size: int, dropout: float = 0.1):
+                 classes: int, num_layers: int, hidden_size: int, dropout: float = 0.1, learnable_pe=True):
         super(ViT, self).__init__()
 
         self.p = patch_size
@@ -188,10 +114,14 @@ class ViT(nn.Module):
         self.hidden_size = hidden_size
         self.dropout = dropout
         self.dropout_layer = nn.Dropout(dropout)
+        self.learnable_pe = learnable_pe
 
         self.embeddings = nn.Linear(self.patch_size, self.embed_size)
         self.class_token = nn.Parameter(torch.randn(1, 1, self.embed_size))
-        self.positional_encoding = nn.Parameter(torch.randn(1, self.num_patches + 1, self.embed_size))
+        if learnable_pe:
+            self.positional_encoding = nn.Parameter(torch.randn(1, self.num_patches + 1, self.embed_size))
+        else:
+            self.positional_encoding = PositionalEncoding2D(self.embed_size, self.num_patches+1)
 
         self.encoders = nn.ModuleList([])
         for layer in range(self.num_layers):
@@ -210,13 +140,21 @@ class ViT(nn.Module):
         x = self.embeddings(x)
 
         b, n, e = x.size()
-
+        
+        if self.learnable_pe:
+            pe = self.positional_encoding
+        else:
+            coords = torch.cartesian_prod(torch.arange(int(h / self.p)), torch.arange(int(w / self.p))).unsqueeze(0).expand(b, int(h * w / (self.p * self.p)), 2) # TODO looks horrible
+            pe = self.positional_encoding(coords).to(x.device)
+        x = x + pe
+        
         class_token = self.class_token.expand(b, 1, e)
         x = torch.cat((x, class_token), dim=1)
-        x = self.dropout_layer(x + self.positional_encoding)
+        
+        x = self.dropout_layer(x)
 
         for encoder in self.encoders:
-            x = encoder(x)
+            x = encoder(x, mask)
 
         x = x[:, -1, :]
 

src/utils/training/image_classification.py
@@ -2,7 +2,7 @@ from .common import *
 from ..plotting import plot_train_test
 
 def train_epoch(model: nn.Module, optimizer: Optimizer, criterion: nn.Module,
-                loader: DataLoader) -> tuple[float, float]:
+                loader: DataLoader, normalise_loss=True) -> tuple[float, float]:
     model.train()
     correct, total_loss, total = 0, 0, 0
     for batch in loader:
@@ -21,9 +21,39 @@ def train_epoch(model: nn.Module, optimizer: Optimizer, criterion: nn.Module,
     
     if total == 0:
         return 0, 1
-    return total_loss, correct / total # NOTE loss not normalised
+    
+    if normalise_loss:
+        total_loss /= total
+    
+    return total_loss, correct / total
 
-def eval(model: nn.Module, criterion: nn.Module, loader: DataLoader) -> tuple[float, float]:
+def train_epoch_coordvit(model: nn.Module, optimizer: Optimizer, criterion: nn.Module,
+                loader: DataLoader, normalise_loss=True) -> tuple[float, float]:
+    model.train()
+    correct, total_loss, total = 0, 0, 0
+    for batch in loader:
+        x, coords, mask, y = batch[0].to(device), batch[1].to(device), batch[2].to(device), batch[3].to(device)
+        
+        optimizer.zero_grad()
+        
+        out = model(x, coords, mask)
+        loss = criterion(out, y)
+        total_loss += loss.item()
+        correct += out.argmax(dim=-1).eq(y).sum().item()
+        total += len(y)
+        
+        loss.backward()
+        optimizer.step()
+    
+    if total == 0:
+        return 0, 1
+    
+    if normalise_loss:
+        total_loss /= total
+    
+    return total_loss, correct / total
+
+def eval(model: nn.Module, criterion: nn.Module, loader: DataLoader, normalise_loss=True) -> tuple[float, float]:
     model.eval()
     correct, total_loss, total = 0, 0, 0
     for batch in loader:
@@ -36,12 +66,33 @@ def eval(model: nn.Module, criterion: nn.Module, loader: DataLoader) -> tuple[fl
         
     if total == 0:
         return 0, 1
-    return total_loss, correct / total # NOTE loss not normalised
+    if normalise_loss:
+        total_loss /= total
+    return total_loss, correct / total
+
+def eval_coordvit(model: nn.Module, criterion: nn.Module, loader: DataLoader, normalise_loss=True) -> tuple[float, float]:
+    model.eval()
+    correct, total_loss, total = 0, 0, 0
+    for batch in loader:
+        x, coords, mask, y = batch[0].to(device), batch[1].to(device), batch[2].to(device), batch[3].to(device)
+        
+        out = model(x, coords, mask)
+        total_loss += criterion(out, y).item()
+        correct += out.argmax(dim=-1).eq(y).sum().item()
+        total += len(y)
+        
+    if total == 0:
+        return 0, 1
+    if normalise_loss:
+        total_loss /= total
+    return total_loss, correct / total
 
 def train_test_loop(model: nn.Module, optimizer: Optimizer, criterion: nn.Module,
                     train_loader: DataLoader, test_loader: DataLoader,
                     num_epochs: int, lr_scheduler: LRScheduler = None,
-                    save_path: str = None, plot=False
+                    save_path: str = None, plot=False,
+                    train_epoch_fn=train_epoch, eval_fn=eval,
+                    normalise_loss=True,
                     ) -> tuple[list[float]]:
     assert save_path is not None or plot == False
     
@@ -60,8 +111,8 @@ def train_test_loop(model: nn.Module, optimizer: Optimizer, criterion: nn.Module
     for i in range(1+len(train_accs), num_epochs+1):
         interval = time()
 
-        train_loss, train_acc = train_epoch(model, optimizer, criterion, train_loader)
-        test_loss, test_acc = eval(model, criterion, test_loader)
+        train_loss, train_acc = train_epoch_fn(model, optimizer, criterion, train_loader, normalise_loss=normalise_loss)
+        test_loss, test_acc = eval_fn(model, criterion, test_loader, normalise_loss=normalise_loss)
         if lr_scheduler is not None:
             lr_scheduler.step(epoch=i, metrics=train_loss)
 